1eaf1637e0c22b2a840a375a5b207fc4d7a87ecc
DNrepo <34350392+DNrepo@users.noreply.github.com>
2018-10-20 20:04:27 +0200

    1 chcekpiont error solved

--- Project5/Liczba_zespolona.cpp
+++ Project5/Liczba_zespolona.cpp
@@ -1,56 +1,27 @@
 #include "Liczba_zespolona.h"
 
-class Liczba_zespolona
-{
-public:
-	Liczba_zespolona();
-	~Liczba_zespolona();
-
-	void IM_set(float im);
-	float IM_get();
-	void RE_set(float re);
-	float RE_get();
-
-private:
-
-	float RE;
-	float IM;
-
-
-};
-
-
+ 
 float Liczba_zespolona::IM_get()
 {
-	return Liczba_zespolona::IM;
+	return IM;
 }
 float Liczba_zespolona::RE_get()
 {
-	return Liczba_zespolona::RE;
+	return RE;
 }
 
 void Liczba_zespolona::IM_set(float im)
 {
-	Liczba_zespolona::IM = im;
+	IM = im;
 }
 void Liczba_zespolona::RE_set(float re)
 {
-	Liczba_zespolona::RE = re;
+	RE = re;
 }
 
 Liczba_zespolona::Liczba_zespolona()
 {
 }
-
-Liczba_zespolona::~Liczba_zespolona()
-{
-}
-
-
-Liczba_zespolona::Liczba_zespolona()
-{
-}
-
 
 Liczba_zespolona::~Liczba_zespolona()
 {
--- Project5/Liczba_zespolona.h
+++ Project5/Liczba_zespolona.h
@@ -1,8 +1,20 @@
 #pragma once
+
 class Liczba_zespolona
 {
 public:
 	Liczba_zespolona();
 	~Liczba_zespolona();
-};
 
+	void IM_set(float im);
+	float IM_get();
+	void RE_set(float re);
+	float RE_get();
+
+private:
+
+	float RE;
+	float IM;
+
+
+};
