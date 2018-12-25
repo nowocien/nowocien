b1ce5cd1b59b8bb36c1105620c5bcbaac47e9282
DNrepo <34350392+DNrepo@users.noreply.github.com>
2018-12-25 13:43:09 +0100

    ready

--- PESEL_PK3/PESEL_PK3/Month.cpp
+++ PESEL_PK3/PESEL_PK3/Month.cpp
@@ -3,28 +3,23 @@
 
 
 const char* cMonth_names[] = {"stycznia", "lutego", "marca", "kwietnia", "maja", "czerwca", "lipca",
-"sierpnia", "wrzesnia", "pazdizernika", "listopada", "grudnia" };
+							  "sierpnia", "wrzesnia", "pazdizernika", "listopada", "grudnia" };
  
-Month::Month(int date)
-{
+
+Month::Month() {
+}
+
+Month::Month(int date){
 	className = (char*)"miesiac";
 	display_length = 2;
 	this->exact_date = date;
 	this->cDisplay_date = new char[display_length];
 }
 
-Month::Month()
-{
-}
 
-void Month::Udpate_offset_and_char_notatnion(int offset)
-{ 
+void Month::Udpate_offset_and_char_notatnion(int offset){ 
 	display_date = offset + exact_date;
 	cDisplay_date = (char*)cMonth_names[display_date-1];
 }
 
-
-Month::~Month()
-{
-	delete cDisplay_date;  
-}
+ 
--- PESEL_PK3/PESEL_PK3/Month.h
+++ PESEL_PK3/PESEL_PK3/Month.h
@@ -3,11 +3,10 @@
 class Month : public Date
 {
 public:
+
 	Month();
 	Month(int);
-	~Month();
 	void Udpate_offset_and_char_notatnion(int offset);
 
-	 
 };
 
